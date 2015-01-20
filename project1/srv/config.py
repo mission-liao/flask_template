439adf85d08ae89f8c5c9417bd4291acfa37ac46
Mission Liao <missionaryliao@gmail.com>
2015-01-20 18:52:25 +0800

    replace postgresql with sqlite
    easier for environment preparation

--- project1/srv/config.py
+++ project1/srv/config.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+from utils import get_root_path
+import os
 
 # TODO: patch it in product
 SECRET_KEY = '?\xbf,\xb4\x8d\xa3"<\x9c\xb0@\x0f5\xab,w\xee\x8d$0\x13\x8b83'
@@ -14,4 +16,4 @@ PRESERVE_CONTEXT_ON_EXCEPTION = True
 CELERY_BROKER_URL = 'amqp://'
 
 # sqlalchemy
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/test'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(get_root_path(), 'test.db')
