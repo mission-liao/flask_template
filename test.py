3ddc9cb5a11c91fcccd3b39c03a2e93e6407fdc6
Mission Liao <missionaryliao@gmail.com>
2015-01-20 18:53:37 +0800

    clean db for testing after finished

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import pytest
+import os
+from project1.srv import utils
+from project1.srv.api import sql
+from project1.srv import model # make sure all table definition are loaded
 
 if __name__ == '__main__':
+    # clear test db
+    db_path = os.path.join(utils.get_root_path(), 'test.db')
+    if os.path.isfile(db_path):
+        os.remove(db_path)
+
+    # make sure all table is created
+    sql.create_all()
+
     pytest.main(['-s', '-v', '--cov=project1', '--cov-config=.converagerc', 'project1/tests'])
+
